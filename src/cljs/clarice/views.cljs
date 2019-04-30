e08a3a5717bb9d8b12110a172b124e2fd1504533
Clarice Bouwer <clarice@bouwer.dev>
2019-04-30 17:05:16 +0200

    Delete unused views ns

--- src/cljs/clarice/views.cljs
+++
@@ -1,9 +0,0 @@
-(ns clarice.views
-  (:require
-    [re-frame.core :as re-frame]
-    [clarice.ui.structure :as ui]
-    [clarice.subs :as subs]))
-
-(defn main-panel []
-  (let [active-panel (re-frame/subscribe [::subs/active-panel])]
-    (ui/panel @active-panel)))
