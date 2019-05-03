414e0b6ada94c7f18c9989193f6711bf72c18a3b
Clarice Bouwer <clarice@bouwer.dev>
2019-05-03 05:02:49 +0200

    Add content to the welcome panel

--- src/cljs/clarice/panels/welcome.cljs
+++ src/cljs/clarice/panels/welcome.cljs
@@ -1,5 +1,12 @@
 (ns clarice.panels.welcome)
 
 (defn panel []
-  [:div
-   [:h1 "Welcome"]])
+  [:div#welcome.panel
+   [:h1 "Hello Clarice."]
+   [:p
+    [:em "/ klarr'is / "]
+    "Clarice is a given name of Latin and Greek origin. The \"Clarice\" spelling is the Germanic "
+    "variant of the Latin Clarus (bright, clear, famous)."]
+   [:p "This Clarice has been writing software for over 15 years. She loves it. "
+    "She makes sure she doesn't get caught up in the perils of that thing called a comfort zone. "
+    "It keeps her excited and continuously learning while enjoying her life-long passion."]])
