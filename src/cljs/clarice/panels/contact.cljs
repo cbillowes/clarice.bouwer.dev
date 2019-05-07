511d38236a319f5985fc6a66ae3204d4be497118
Clarice Bouwer <clarice@bouwer.dev>
2019-05-07 04:24:39 +0200

    Add content to the contact panel

--- src/cljs/clarice/panels/contact.cljs
+++ src/cljs/clarice/panels/contact.cljs
@@ -1,5 +1,33 @@
-(ns clarice.panels.contact)
+(ns clarice.panels.contact
+  (:require
+    [clarice.helpers :refer [anchor]]))
 
 (defn panel []
-  [:div
-   [:h1 "Contact"]])
+  [:div#contact
+   [:h1 "Get in touch with me"]
+   [:div.icons
+    (anchor [:i.fab.fa-twitter]
+            "https://twitter.com/cbillowes"
+            {:title "I tweet @cbillowes"
+             :class "twitter"})
+
+    (anchor [:i.fab.fa-github]
+            "https://github.com/cbillowes"
+            {:title "I push code to GitHub"
+             :class "github"})
+
+    (anchor [:i.fab.fa-linkedin-in]
+            "https://www.linkedin.com/in/cbouwer/"
+            {:title "My Linked In profile"
+             :class "linkedin"})
+
+    (anchor [:i.fas.fa-blog]
+            "https://curiousprogrammer.dev"
+            {:title "I blog at Curious Programmer"
+             :class "blog"})
+
+    (anchor [:i.fas.fa-envelope-open-text]
+            "mailto:clarice.bouwer.dev"
+            {:title    "Get in touch with me"
+             :same-tab true
+             :class    "email"})]])
