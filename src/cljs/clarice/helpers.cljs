16c4f61378418109206a2ecd3616d2ce85dcae36
Clarice Bouwer <clarice@bouwer.dev>
2019-05-09 19:57:56 +0200

    Enable anchors to open href in _blank or _parent tab.

--- src/cljs/clarice/helpers.cljs
+++ src/cljs/clarice/helpers.cljs
@@ -1,11 +1,11 @@
 (ns clarice.helpers)
 
 (defn anchor
-  [content link & [{:keys [class title]}]]
+  [content link & [{:keys [class title same-tab]}]]
   [:a {:title title
        :class class
        :href link
-       :target "_blank"
+       :target (if (true? same-tab) "_parent" "_blank")
        :rel "nofollow noopener noreferrer"}
    content])
 
